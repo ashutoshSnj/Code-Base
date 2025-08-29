2b93e84f48df7dcf3528a69c3b3467e6761b62e7
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-08-29 08:34:15 +0530

    challange solve

--- CodingTest/First.java
+++ CodingTest/First.java
@@ -2,6 +2,17 @@ package start.build.CodingTest;
 
 public class First {
  public static void main(String[] args) {
+	 /*
+	  *  987654321
+	  *  87654321
+	  *  7654321
+	  *  654321
+	  *  54321
+	  *  4321
+	  *  321
+	  *  21
+	  *  1 
+	  */
 	 for(int i=9;i>=1;i--) {
 		for(int j=i;j>=1;j--) {
 			System.out.print(j+" ");
--- CodingTest/Nigne.java
+++ CodingTest/Nigne.java
@@ -2,6 +2,12 @@ package start.build.CodingTest;
 
 public class Nigne {
   public static void main(String[] args) {
+	  /*
+	   * 1
+	   * 12
+	   * 123
+	   * 1234
+	   */
 	for(int i=1;i<=4;i++) {
 		for(int j=1;j<=i;j++) {
 			System.out.println(j+" ");
